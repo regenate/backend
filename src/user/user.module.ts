73501bca4917057fbe58b53c3c6f399f20f1f758
John Opaluwa <johnopaluwa@gmail.com>
2022-05-08 23:10:52 +0200

    add mentor and mentee controller to module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { UploaderModule } from '@src/uploader/uploader.module';
 import { UtilModule } from '@src/util/util.module';
 import { userProviders } from './providers';
-import { UserController } from './user.controller';
+import {
+  MenteeController,
+  MentorController,
+  UserController,
+} from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   providers: [...userProviders, UserService],
   imports: [UtilModule, UploaderModule],
-  controllers: [UserController],
+  controllers: [UserController, MentorController, MenteeController],
   exports: [UserService],
 })
 export class UserModule {}
